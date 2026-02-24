598a06e19a96cfa49ca99045e3eadea87931ca5c
Sulyman Abubakar <105800274+Ludius1@users.noreply.github.com>
2026-02-24 20:41:31 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
         <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
             <div className="text-[#414141]">
                 <div className="flex items-center gap-2">
-                    <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>EXPLORE COUNTRY</p>
+                    <p className='w-8 md:w-11 h-[2px] bg-red-500'></p>
+                    <p className='font-medium text-sm md:text-base '>EXPLORE COUNTRY</p>
                 </div>
-                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Wines Collection</h1>
+                <h1 className='prata-regular text-3xl  sm:py-3  lg:text-5xl leading-relaxed'>Wines Collection</h1>
                 <div className="flex items-center gap-2 ">
                         <p className='font-semibold text-sm md:text-base'> SHOP NOW</p>
-                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='w-8 md:w-11 h-[2px] bg-red-500'></p>
                 </div>
             </div>
 
@@ -28,30 +28,18 @@ const Hero = () => {
 
     </div>
 
-    
-
-    <div className="my-10 flex flex-col md:flex-row gap-16">
-              <img className='w-full md:max-w-[450px] h-[300px] object-cover aspect-video' src={assets.about_img} alt="" />
-
-        
-                        
-                              <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
-                    <p><div className="text-2xl text-center pt-8 border-t">
-                         <Title text1={'ABOUT'} text2={'US'}/>
-                           </div></p>
-                    <p>Welcome to Winelady, where passion for exceptional wines meets the joy of discovery. We curate a diverse collection of wines from around the world, bringing you unique flavors, exquisite aromas, and unforgettable experiences.</p>
-                    <p>
-                         <button to='/collection' className=' bg-black text-white px-8 py-3 text-sm active:bg-gray-700 '>
-                                       <p>EXPLORE OUR COLLECTION</p>
+             <div className="text-center py-8 text-3xl ">
+            <Title text1={'ABOUT'} text2={'US'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>Welcome to Winelady, where passion for exceptional wines meets the joy of discovery. We curate a diverse collection of wines from around the world, bringing you unique flavors, exquisite aromas, and unforgettable experiences</p>
+             <p className='py-5'>
+                         <button to='/collection' className='   bg-red-500 text-white px-8 py-3 text-sm active:bg-gray-700 '>
+                                       <p className=''>EXPLORE OUR COLLECTION</p>
                                        <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
                                    </button>
                     </p>
 
+        </div>
 
-              </div>
-          
-        
-          </div>
 
 
     </div>
