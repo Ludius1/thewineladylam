949220738ff4c4be27ed2aa3df73bf3445b6df2c
Sulyman Abubakar <105800274+Ludius1@users.noreply.github.com>
2026-02-27 13:58:22 +0100

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const Newsletter = () => {
 
@@ -7,13 +9,19 @@ const Newsletter = () => {
     }
   return (
     <div className='text-center'>
-         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-         <p className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, reiciendis.</p>
+         <p className='text-2xl font-medium text-gray-800'>Get special deal when you order 5 cases.</p>
+         <p className="text-gray-400 mt-3">Stock up, save more, and keep your favorites flowing.</p>
 
-         <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 '>
-                <input className='w-full sm:flex-1 outline-none'required type="email"  placeholder='Enter your email'/>
-                <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
-         </form>
+              
+              <Link 
+  to="/collection" 
+  className=" sm:w-1/3 flex items-center justify-center mx-auto my-6 bg-red-500 text-white text-xs px-10 py-4"
+>
+  SECURE YOUR DEAL NOW BEFORE IT’S GONE.
+
+</Link>
+                
+        
     </div>
   )
 }
