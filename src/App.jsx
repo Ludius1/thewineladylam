0a547233e9ce3d74edc56ca2741a22200b8b035f
Sulyman Abubakar <105800274+Ludius1@users.noreply.github.com>
2026-03-18 13:16:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -11,28 +11,38 @@ import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Order'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Searchbar from './components/Searchbar'
-import { ToastContainer, toast } from 'react-toastify';
+import AgeVerification from './components/AgeVerification' // Import the new component
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
-      <ToastContainer/>
-      <Navbar/>
-      <Searchbar/>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      
+      {/* 1. Age Verification overlay */}
+      <AgeVerification /> 
+
+      {/* 2. Navigation */}
+      <Navbar />
+
+      {/* Note: <Searchbar /> was removed here because we are now 
+          using the inline input inside the Navbar component.
+      */}
+
       <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/collection' element={<Collection/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/product/:productId' element={<Products/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/place-order' element={<PlaceOrder/>}/>
-          <Route path='/order' element={<Order/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/product/:productId' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/place-order' element={<PlaceOrder />} />
+          <Route path='/order' element={<Order />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
     </div>
   )
 }
